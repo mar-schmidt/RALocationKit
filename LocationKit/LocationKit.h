cd645f24de66ad7bfedf22c6f3d226c4dad0d911
”Marcus <marcus.ronelius@gmail.com>
2016-01-14 22:31:40 +0100

    Initial Commit

---
+++ LocationKit/LocationKit.h
@@ -0,0 +1,19 @@
+//
+//  LocationKit.h
+//  LocationKit
+//
+//  Created by Marcus Ronélius on 2016-01-14.
+//  Copyright © 2016 Ronelium Applications. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LocationKit.
+FOUNDATION_EXPORT double LocationKitVersionNumber;
+
+//! Project version string for LocationKit.
+FOUNDATION_EXPORT const unsigned char LocationKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocationKit/PublicHeader.h>
+
+
